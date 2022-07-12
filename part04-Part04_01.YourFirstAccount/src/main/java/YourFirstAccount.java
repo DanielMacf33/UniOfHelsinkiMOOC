0ee881f995a1565a6e75ac2291a227f085e2110a
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-07-12 13:45:23 +0100

    completed part04_01 and 02

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,10 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        
+        Account Daniel = new Account("Daniel", 100);
+        
+        Daniel.deposit(20.00);
+        System.out.println(Daniel);
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -2,7 +2,14 @@
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+        
+        Account MatthewsAccount = new Account ("Matthews account", 1000.00);
+        Account MyAccount = new Account ("My account", 0);
+        
+        MatthewsAccount.withdrawal(100.00);
+        MyAccount.deposit(100.00);
+        
+        System.out.println(MatthewsAccount);
+        System.out.println(MyAccount);
     }
 }
