f4fc0388357cdad27e20edd236dfeee9c812b4fd
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-07-13 19:20:28 +0100

    finished part04_05

--- part04-Part04_04.Room/src/main/java/Room.java
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -9,5 +9,13 @@
  * @author Daniel
  */
 public class Room {
-    
+
+    private String code;
+    private int seats;
+
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+
+    }
 }
