bb1cb401d69df445bce0f962466fed8031b812e5
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-07-18 01:23:02 +0100

    completed part04_17

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -10,5 +10,21 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Name: ");
+            String item = scanner.nextLine();
+            if (item.isEmpty()) {
+                break;
+            }
+
+            // Add to the list a new person
+            // whose name is the previous user input
+            items.add(new Item(item));
+        }
+        
+        for(Item item: items){
+            System.out.println(item);
+        }
+
     }
 }
