c3becaaa6a54dc941de42e189ae8d247c1b4cece
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-07-21 19:26:09 +0100

    finished part04_25 - 26

--- part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -12,23 +12,32 @@ public class GuestListFromAFile {
         String file = scanner.nextLine();
 
         ArrayList<String> list = new ArrayList<>();
-        // implement reading the file here.
-        System.out.println("");
+        try ( Scanner sc = new Scanner(Paths.get(file))) {
 
-        System.out.println("Enter names, an empty line quits.");
-        while (true) {
-            String name = scanner.nextLine();
-            if (name.isEmpty()) {
-                break;
+            // we read all the lines of the file
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+                list.add(row);
             }
-
-            if (list.contains(name)) {
-                System.out.println("The name is on the list.");
-            } else {
-                System.out.println("The name is not on the list.");
-            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("");
         }
 
-        System.out.println("Thank you!");
+            System.out.println("Enter names, an empty line quits.");
+            while (true) {
+                String name = scanner.nextLine();
+                if (name.isEmpty()) {
+                    break;
+                }
+
+                if (list.contains(name)) {
+                    System.out.println("The name is on the list.");
+                } else {
+                    System.out.println("The name is not on the list.");
+                }
+            }
+
+            System.out.println("Thank you!");
+        }
     }
-}
