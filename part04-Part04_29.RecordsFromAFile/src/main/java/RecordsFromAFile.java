f2d6ad5e7f7c5986beb2445620a11374e9adb96a
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-07-25 19:33:09 +0100

    finished part04_29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,11 +1,29 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class RecordsFromAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name of the file: ");
+        String nameFile = scanner.nextLine();
+        
+        try (Scanner sc = new Scanner(Paths.get(nameFile))) {
+
+    while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+
+        String[] parts = line.split(",");
+        String name = parts[0];
+        int age = Integer.valueOf(parts[1]);
+
+        System.out.println(name + ", " + "age: " + age + " years");
+       
+    }
+}
 
     }
 }
