83d994a675599615884d8facc95706001fab08d7
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-07-25 19:24:45 +0100

    finished part04_28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,18 +1,44 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
+   
+      
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
-        System.out.print("File? ");
+        int count = 0;
+
+        System.out.println("File? ");
         String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
+        System.out.println("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
+        System.out.println("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+
+        try ( Scanner fileScan = new Scanner(Paths.get(file))) {
+            while (fileScan.hasNextLine()) {
+                list.add(fileScan.nextLine());
+
+                
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        for (String item : list){
+            int number = Integer.valueOf(item);
+            
+            if (number >= lowerBound && number <= upperBound) {
+                    count++;
+                }
+        }
+        
+        System.out.println("Numbers: " + count);
 
     }
 
