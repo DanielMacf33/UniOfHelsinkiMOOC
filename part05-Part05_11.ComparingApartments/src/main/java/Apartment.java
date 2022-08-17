02ebce783799922812be30f9d87ee59ae14a43ca
DanielMacf33 <danielmacfarlane33@hotmail.co.uk>
2022-08-17 17:54:33 +0100

    finished part 05_11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -1,4 +1,7 @@
 
+import static java.lang.Math.abs;
+
+
 public class Apartment {
 
     private int rooms;
@@ -10,5 +13,33 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public boolean largerThan(Apartment compared){
+        if(this.squares < compared.squares){
+            return false;
+        }
+        return true;
+        
+    }
+    
+    public int priceDifference(Apartment compared){
+        int priceOne = this.squares * this.princePerSquare;
+        int priceTwo = compared.squares * compared.princePerSquare;
+        
+        return abs(priceOne - priceTwo);
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared){
+        int priceOne = this.squares * this.princePerSquare;
+        int priceTwo = compared.squares * compared.princePerSquare;
+        
+        if(priceOne > priceTwo){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
 
 }
